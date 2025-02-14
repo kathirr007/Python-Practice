4ddac8a1f22fb6ca7ba75c1a6e840625566ca6ed
Kathiravan K <kathirr007@gmail.com>
2025-02-14 08:41:54 +0530

    updated gitignore to ignore virtual environments

---
+++ tuples.py
@@ -0,0 +1,13 @@
+"""
+Tuples are like list uses parenthesis
+"""
+
+my_tuple = (1, 4, 3, 5, 70, 32, 14)
+
+print(my_tuple[3])
+print(my_tuple[-1])  # ordered from the last item from -1
+print(my_tuple[-5])
+
+# print(my_tuple[7])  # IndexError: tuple index out of range
+
+# my_tuple[1] = "Kathir"  # TypeError: 'tuple' object does not support item assignment
